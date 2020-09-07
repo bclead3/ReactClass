fbd444ee8f21cbba20d055c356451901b29fe1ec
Ben Leadholm <leadh009@umn.edu>
2020-09-06 23:38:58 -0500

    Componentize for Exercise 2 for React

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -36,16 +36,13 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag='li'>
-                    <Media left middle>
-                        <Media object src={dish.image} />
-                    </Media>
-                    <Media body className='ml-5'>
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card onClick={() => this.onDishSelect(dish)}>
+                    <CardImg width='100%' src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
               </div>
             );
         });
@@ -53,12 +50,10 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                     {menu}
                 </div>
                 <div className="row">
-                  <div  className="col-12  m-1">
+                  <div  className="col-12 col-md-5 m-1">
                     {this.renderDish(this.state.selectedDish)}
                   </div>
                 </div>
