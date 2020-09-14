e2bba24d1ec05d75472191617f3fed273bb5d670
Ben Leadholm <leadh009@umn.edu>
2020-09-14 00:02:09 -0500

    Single Page Application part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+
 // import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
+
+    const HomePage = () => {
+        return(
+          <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+        );
+    }
+
     return(
       <div className="MainApp">
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-        {/* <DishDetail selectedDish={ this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] } />  */}
+        <Header />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path='/contactus' component={Contact} />} />
+            <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
