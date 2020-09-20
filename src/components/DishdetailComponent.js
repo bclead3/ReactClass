6e8d42996ca62bff94e7b43a99fe001dcb6978b6
Ben Leadholm <leadh009@umn.edu>
2020-09-20 12:19:56 -0500

    Assignment 3: Uncontrolled (Modal) form w/ validation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';  //CardImgOverlay, 
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 
@@ -12,86 +12,55 @@ class DishDetail extends Component {
             selectedDish: null,
             comments: COMMENTS,
             dishComments: [],
-            isModalOpen: false
+            isModalOpen: false,
+            rating: 5,
+            username: '',
+            comment: '',
+            touched: {
+                rating: false,
+                username: false,
+                comment: false
+            }
         }
 
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    toggleModal() {
+    handleBlur = (field) => (evt) => {
         this.setState({
-            isModalOpen: !this.state.isModalOpen
+            touched: { ...this.state.touched, [field]: true }
+        });
+    }
+
+    handleInputChange() {
+        // const target = event.target;
+        const value = this.username.value;
+
+        this.setState({
+          username: value
         });
     }
 
     handleSubmit(event) {
-        // alert( JSON.stringify(event) );
-        alert( "rating: " +
-            this.rating.value +
-            " username: " +
-            this.username.value +
-            " comment: " +
-            this.comment.value
-    );
+        const log_string = "{rating: '" +
+        this.rating.value +
+        "', username: '" +
+        this.username.value +
+        "', comment: '" +
+        this.comment.value +
+        "'}"
 
         this.toggleModal();
+        alert( log_string );
+        console.log(log_string);
         event.preventDefault();
     }
 
-    renderDishDetail(selectedDish) {
-        if (selectedDish != null) {
-            return(
-                <div className='col-12 col-md-5 m1'>
-                    <Card>
-                        <CardImg top src={selectedDish.image} alt={selectedDish.name} />
-                        <CardBody>
-                            <CardTitle>{selectedDish.name}</CardTitle>
-                            <CardText>{selectedDish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        }
-        else {
-            return(<div/>);
-        }
-    }
-
-    renderComments(comments) {
-        if (comments != null) {
-            // this.setState({dishComments: []});
-            this.state.dishComments = [];
-            comments.map(comment => {
-                if (comment.dishId === this.props.selectedDish.id) {
-                    this.state.dishComments.push(comment)
-                }
-            });
-            return(
-                <div className='col-12 col-md-5 m1'>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {this.state.dishComments.map((comment) => {
-                            return(
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author} , 
-                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
-                                        .format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <Button onClick={this.toggleModal}><span className="fa fa-edit fa-lg"></span> Submit Comment</Button>
-                </div>
-            );
-        }
-        else {
-            return(<div></div>);
-        }
-    }
-
     render() {
+        const errors = this.validate(this.state.username);
+
         if (this.props.selectedDish == null) {
             return(<div></div>);
         }
@@ -129,8 +98,18 @@ class DishDetail extends Component {
                                 </FormGroup>
                                 <FormGroup>
                                     <Label htmlFor="username">Username</Label>
-                                    <Input type="text" id="username" name="username"
-                                        innerRef={(input) => this.username = input} />
+                                    <Input type="text"
+                                        id="username"
+                                        name="username"
+                                        placeholder="User Name"
+                                        value={this.state.username}
+                                        valid={errors.username === ''}
+                                        invalid={errors.username !== ''}
+                                        onBlur={this.handleBlur('username')}
+                                        onChange={this.handleInputChange}
+                                        innerRef={(input) => this.username = input}
+                                        />
+                                    <FormFeedback>{errors.username}</FormFeedback>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label htmlFor="comment">Comment</Label>
@@ -144,6 +123,77 @@ class DishDetail extends Component {
                 </div>
             );
         }
+    }
+
+    renderComments(comments) {
+        if (comments != null) {
+            // this.setState({dishComments: []});
+            this.state.dishComments = [];
+            comments.map(comment => {
+                if (comment.dishId === this.props.selectedDish.id) {
+                    this.state.dishComments.push(comment)
+                }
+            });
+            return(
+                <div className='col-12 col-md-5 m1'>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {this.state.dishComments.map((comment) => {
+                            return(
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} ,
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                                        .format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <Button onClick={this.toggleModal} className='bg-white text-muted'><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+                </div>
+            );
+        }
+        else {
+            return(<div></div>);
+        }
+    }
+
+    renderDishDetail(selectedDish) {
+        if (selectedDish != null) {
+            return(
+                <div className='col-12 col-md-5 m1'>
+                    <Card>
+                        <CardImg top src={selectedDish.image} alt={selectedDish.name} />
+                        <CardBody>
+                            <CardTitle>{selectedDish.name}</CardTitle>
+                            <CardText>{selectedDish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else {
+            return(<div/>);
+        }
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    validate(username) {
+        const errors = {
+            username: ''
+        };
+
+        if (this.state.touched.username && username.length < 3)
+            errors.username = 'User Name should be greater than 2 characters';
+        else if (this.state.touched.username && username.length > 15)
+            errors.username = 'User Name should 15 characters or less';
+
+        return errors;
     }
 }
 
