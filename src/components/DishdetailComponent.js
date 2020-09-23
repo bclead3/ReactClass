7f083edb3579230f6db1de50778bed756e426bf6
Ben Leadholm <leadh009@umn.edu>
2020-09-23 08:37:05 -0500

    Updated DishdetailComponent, and divvyd-up redux resources

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,199 +1,178 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, FormFeedback } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { COMMENTS } from '../shared/comments';
+
+function RenderDish({dish}) {
+    if (dish != null) {
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+    else {
+        return(<div/>);
+    }
+}
+
+function RenderComments({comments}) {
+    if (comments != null) {
+        return(
+            <ul className="list-unstyled">
+                {comments.map((comment) => {
+                    return(
+                        <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} ,
+                                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                                .format(new Date(Date.parse(comment.date)))}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+    else {
+        return(<div></div>);
+    }
+}
 
 class DishDetail extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
-            selectedDish: null,
-            comments: COMMENTS,
-            dishComments: [],
-            isModalOpen: false,
-            rating: 5,
-            username: '',
-            comment: '',
-            touched: {
-                rating: false,
-                username: false,
-                comment: false
-            }
+            isModalOpen: false
         }
-
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
     }
-
-    handleBlur = (field) => (evt) => {
-        this.setState({
-            touched: { ...this.state.touched, [field]: true }
-        });
-    }
-
-    handleInputChange() {
-        // const target = event.target;
-        const value = this.username.value;
-
-        this.setState({
-          username: value
-        });
-    }
-
-    handleSubmit(event) {
-        const log_string = "{rating: '" +
-        this.rating.value +
-        "', username: '" +
-        this.username.value +
-        "', comment: '" +
-        this.comment.value +
-        "'}"
-
-        this.toggleModal();
-        alert( log_string );
-        console.log(log_string);
-        event.preventDefault();
-    }
-
-    render() {
-        const errors = this.validate(this.state.username);
-
-        if (this.props.selectedDish == null) {
-            return(<div></div>);
-        }
-        else {
+   
+    render(){
+        const dish=this.props.selectedDish;
+        if(dish != null){
             return (
                 <div className="container">
                     <div className="row">
                         <Breadcrumb>
                             <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{this.props.selectedDish.name}</BreadcrumbItem>
+                            <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
                         </Breadcrumb>
                         <div className="col-12">
-                            <h3>{this.props.selectedDish.name}</h3>
+                            <h3>{dish.name}</h3>
                             <hr />
-                        </div>                
+                        </div>
                     </div>
                     <div className="row">
-                        {this.renderDishDetail(this.props.selectedDish)}
-                        {this.renderComments(this.state.comments)}
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish = {dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            <RenderComments comments={COMMENTS.filter(comment => comment.dishId === dish.id)} />
+                            <CommentForm></CommentForm>
+                        </div>
                     </div>
-                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                        <ModalHeader toggle={this.toggleModal}>Submit a Comment</ModalHeader>
-                        <ModalBody>
-                            <Form onSubmit={this.handleSubmit}>
-                                <FormGroup>
-                                    <Label htmlFor="rating">Rating</Label>
-                                    <Input type="select" id="rating" name="rating"
-                                        innerRef={(input) => this.rating = input } >
-                                            <option>5</option>
-                                            <option>4</option>
-                                            <option>3</option>
-                                            <option>2</option>
-                                            <option>5</option>
-                                    </Input>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor="username">Username</Label>
-                                    <Input type="text"
-                                        id="username"
-                                        name="username"
-                                        placeholder="User Name"
-                                        value={this.state.username}
-                                        valid={errors.username === ''}
-                                        invalid={errors.username !== ''}
-                                        onBlur={this.handleBlur('username')}
-                                        onChange={this.handleInputChange}
-                                        innerRef={(input) => this.username = input}
-                                        />
-                                    <FormFeedback>{errors.username}</FormFeedback>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor="comment">Comment</Label>
-                                    <Input type="textarea" id="comment" name="comment"
-                                    innerRef={(input) => this.comment = input}  />
-                                </FormGroup>
-                                <Button type="submit" value="submit" color="primary">Submit</Button>
-                            </Form>
-                        </ModalBody>
-                    </Modal>
                 </div>
-            );
-        }
-    }
-
-    renderComments(comments) {
-        if (comments != null) {
-            // this.setState({dishComments: []});
-            this.state.dishComments = [];
-            comments.map(comment => {
-                if (comment.dishId === this.props.selectedDish.id) {
-                    this.state.dishComments.push(comment)
-                }
-            });
-            return(
-                <div className='col-12 col-md-5 m1'>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {this.state.dishComments.map((comment) => {
-                            return(
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author} ,
-                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
-                                        .format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <Button onClick={this.toggleModal} className='bg-white text-muted'><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
-                </div>
-            );
+            )
         }
         else {
-            return(<div></div>);
-        }
-    }
-
-    renderDishDetail(selectedDish) {
-        if (selectedDish != null) {
-            return(
-                <div className='col-12 col-md-5 m1'>
-                    <Card>
-                        <CardImg top src={selectedDish.image} alt={selectedDish.name} />
-                        <CardBody>
-                            <CardTitle>{selectedDish.name}</CardTitle>
-                            <CardText>{selectedDish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
+            return (
+                <div></div>
             );
         }
-        else {
-            return(<div/>);
+    }
+}
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+    
+class CommentForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
         }
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    toggleModal() {
+    toggleModal(){
         this.setState({
-            isModalOpen: !this.state.isModalOpen
+            isModalOpen : !this.state.isModalOpen
         });
     }
 
-    validate(username) {
-        const errors = {
-            username: ''
-        };
+    handleSubmit(values) {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+    }
 
-        if (this.state.touched.username && username.length < 3)
-            errors.username = 'User Name should be greater than 2 characters';
-        else if (this.state.touched.username && username.length > 15)
-            errors.username = 'User Name should 15 characters or less';
-
-        return errors;
+    render(){
+        return (
+            <div>
+            <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-edit fa-lg"></span>Submit Comment
+                                    </Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={4}>Rating</Label>
+                        </Row>
+                        <Row>   
+                            <Col>
+                                <Control.select model=".rating" className="form-control"name="rating">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="yourname" md={4}>Your Name</Label>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Control.text model=".yourname" id="yourname" name="yourname"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }} />
+                                <Errors
+                                    className="text-danger"
+                                    model=".yourname"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }} />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="comment" md={2}>Comment</Label></Row>
+                            <Row>
+                            <Col>
+                                <Control.textarea model=".comment" id="comment" name="comment" rows="6"
+                                    className="form-control" />
+                            </Col>
+                        </Row>
+                        <Button type="submit" value="submit" color="primary" onClick={this.toggleModal}>Submit</Button>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+            </div>
+        )
     }
 }
 
